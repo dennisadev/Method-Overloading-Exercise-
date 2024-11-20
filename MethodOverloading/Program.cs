5e2da5651e323bbc5028e27d85470b79615615cf
Dennis <dennisadev@gmail.com>
2024-11-20 14:56:44 -0500

    inital commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace MethodOverloading
+﻿using System.Text.RegularExpressions;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int numOne, int numTwo)
+        { 
+            return numOne + numTwo; 
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+        
+        public static string Add(int numOne, int numTwo, bool isCurrency)
+        {
+            var sum = numOne + numTwo;
+
+            if (isCurrency && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCurrency && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else 
+            { 
+                return sum.ToString();
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(2, 3, true));
+            Console.WriteLine(Add(1, 0, true));
+            Console.WriteLine(Add(8, 8, true));
+            Console.WriteLine(Add(8, 8, false));
         }
     }
 }
